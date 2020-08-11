13cbc7484aae74760e3b5d611094afaf569a7483
barbichola0899 <barbichola0899@users.noreply.github.com>
2020-08-11 18:56:59 -0300

    update

--- CNN.py
+++ CNN.py
@@ -10,17 +10,29 @@ import matplotlib.pyplot as plt
 import os
 plt.style.use('fivethirtyeight')
 
-images_path = 'C:/Users/lucas/Documents/GitHub/Pokedex/Pokemon Pictures/'
+images_path = 'C:/Users/lucas/Documents/GitHub/Pokedex/Pokemon Pictures'
 pokemons = os.listdir(images_path)
 num_pokemons = 151
 classes = np.identity(num_pokemons)
-
-for i in range(10):
+train = []
+test = []
+for i in range(num_pokemons):
     pokemon = pokemons[i]
-    pokeimgs = os.listdir('/'.join([images_path,pokemon])+'/')
-    for j in range(1,4):
-        img = pokeimgs[j]
-        print(img)
-        imgdata = cv.imread('/'.join([images_path,pokemon,img])+'/')
-        print(imgdata)
-    print('\n')
+    try:
+        pokeimgs = os.listdir('/'.join([images_path,pokemon])+'/')
+    
+        for j in range(1,11):
+            img = pokeimgs[j]
+            imgdata = cv.imread('/'.join([images_path,pokemon,img]))
+            if j <= 7:
+                train.append(imgdata)
+            else:
+                test.append(imgdata)
+    except:
+        pass
+
+train = np.array(train).reshape()
+
+print(len(train))
+print(len(test))
+        
